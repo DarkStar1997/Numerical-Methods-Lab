b8b7da3b53e525ebfce19e8743d8eef10f8943f9
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-04-24 05:46:07 +0000

    Update weddle.c

--- Assignment 8/weddle.c
+++ Assignment 8/weddle.c
@@ -3,18 +3,13 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    double x[n];
-    double y[n];
-    int i;
+    double x[n], y[n];
     printf("Enter the value of x and y :\n");
-    for(i=0;i<=n;i++)
-    {
-        scanf("%lf%lf",&x[i],&y[i]);
-    }
+    for(int i=0;i<n;i++)
+        scanf("%lf %lf",x+i,y+i);
     double h=x[1]-x[0];
     double ans=(y[0]+y[n]);
     for(i=1;i<n;i++)
-    {
         if((i%6)==0)
             ans+=2*y[i];
         else if(i%6==3)
@@ -23,7 +18,6 @@ int main()
                 ans+=y[i];
         else
             ans+=(5*(y[i]));
-    }
     ans=ans*(0.3*h);
     printf("ans =%lf",ans);
     return 0;
