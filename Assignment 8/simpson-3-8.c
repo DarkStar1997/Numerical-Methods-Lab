592f1c74c0c870743c16b7c44da965b52e8ddbb8
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-04-24 05:44:27 +0000

    Update simpson-3-8.c

--- Assignment 8/simpson-3-8.c
+++ Assignment 8/simpson-3-8.c
@@ -3,23 +3,17 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    double x[n];
-    double y[n];
-    int i;
+    double x[n], y[n];
     printf("Enter the value of x and y :\n");
-    for(i=0;i<=n;i++)
-    {
-        scanf("%lf%lf",&x[i],&y[i]);
-    }
+    for(int i=0;i<n;i++)
+        scanf("%lf%lf",x+i,y+i);
     double h=x[1]-x[0];
     double ans=(y[0]+y[n]);
     for(i=1;i<n;i++)
-    {
         if((i%3)==0)
             ans+=(2*(y[i]));
         else
             ans+=(3*(y[i]));
-    }
     ans=ans*(0.375*h);
     printf("ans =%lf",ans);
     return 0;
